198a7e282a4cd18b08a7b508ed80847546f80093
Damian Arturo Garces Paez <guners86@hotmail.com>
2025-08-29 16:14:20 +0400

    Add password hashing utility and update createAccount handler to hash user passwords

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 import User from "../models/User";
+import { hashPassword } from "../utils/auth";
 
 export const createAccount = async (req: Request, res: Response) => {
   try {
 
-    const { email } = req.body;
+    const { email, password } = req.body;
     const userExist = await User.findOne({ email });
 
     if (userExist) {
@@ -12,6 +13,7 @@ export const createAccount = async (req: Request, res: Response) => {
     }
 
     const user = new User(req.body);
+    user.password = await hashPassword(password);
     await user.save();
     res.status(201).send('User created successfully');
   } catch (error) {
