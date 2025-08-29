f3da9cc1f97fdfac33b773bcea69802c777c2284
Damian Arturo Garces Paez <guners86@hotmail.com>
2025-08-29 17:14:53 +0400

    Add validation for user registration fields in createAccount handler

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,9 +2,16 @@ import { Request, Response } from "express";
 import User from "../models/User";
 import { hashPassword } from "../utils/auth";
 import slug from "slug";
+import { validationResult } from "express-validator";
 
 export const createAccount = async (req: Request, res: Response) => {
   try {
+
+    // Validation Handlers
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
     const { email, password } = req.body;
     const userExist = await User.findOne({ email });
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
 import { createAccount } from './handlers';
 
 const router = Router();
 
 /** User Registration */
-router.post('/auth/register', createAccount);
+router.post('/auth/register', 
+  body('name').notEmpty().withMessage('Name is required'),
+  body('email').isEmail().withMessage('Must be a valid email address'),
+  body('password').isLength({ min: 6 }).withMessage('Must be at least 6 characters long'),
+  body('handle').notEmpty().withMessage('Handle is required'),
+  createAccount);
 
 export default router
