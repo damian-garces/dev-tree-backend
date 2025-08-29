e0f2b3be116117bbade70a28190c11508700cead
Damian Arturo Garces Paez <guners86@hotmail.com>
2025-08-29 15:25:54 +0400

    Refactor user registration endpoint to handle asynchronous user creation

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import User from './models/User';
 
 const router = Router();
 
 /** User Registration */
-router.post('/auth/register', (req, res) => {
+router.post('/auth/register', async (req, res) => {
   console.log(req.body);
+  const user = new User(req.body);
+  await user.save();
   res.send('Register Endpoint');
 })
 
