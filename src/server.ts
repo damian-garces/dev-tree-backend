c2280daac5df1a1669ce894d1d269bbdd03ea4ec
Damian Arturo Garces Paez <guners86@hotmail.com>
2025-09-08 23:15:53 +0400

    Add CORS support with configurable options

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,15 @@ import express from "express"
 import 'dotenv/config';
 import router from "./router"
 import { connectDB } from "./config/db"
+import cors from 'cors'
+import { corsOptions } from "./config/cors";
 
 const app = express()
 
 connectDB()
+
+// CORS
+app.use(cors(corsOptions))
 
 /// read json body
 app.use(express.json())
